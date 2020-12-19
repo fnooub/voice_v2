a99a140dc80d2989cd9f2ac279e5a29e752e0ea4
fnooub <61742373+fnooub@users.noreply.github.com>
2020-12-19 19:23:11 +0700

    add remove

--- get.php
+++ get.php
@@ -76,6 +76,9 @@ elseif ($flag == 'kh') {
 	$noidung = get_row('<div class="story-content.+?">', '<div class="w-100 center-block center-text">', $str);
 }
 
+// remove ads
+$noidung = preg_replace(array('/Bạn đang đọc truyện được copy tại/iu', '/Bạn đang đọc truyện được lấy tại/iu', '/Bạn đang đọc truyện tại/iu', '/Text được lấy tại/iu', '/nguồn truyện\s*:/iu', '/nguồn\s*:/iu', '/chấm cơm\.?/iu', '/www\s*\./i', '/https?\s*:\s*\/?\/?/i', '/\.\s*vn/i', '/\.\s*com/iu', '/Truyen\s*FULL/iu', '/truyện\s*full/iu', '/Đọc Truyện Online Tại/iu', '/Đọc Truyện Kiếm Hiệp Hay Nhất\:?/iu', '/Truyện.{1,10}Hiệp/iu', '/truyenyy/iu', '/Truyện\s*YY/iu'), '', $noidung);
+
 /**
  * output
  */
